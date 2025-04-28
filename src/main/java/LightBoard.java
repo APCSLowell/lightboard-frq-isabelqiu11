c154441208a2129bb782e87e0a775047433187a8
isabelqiu11 <qiu.isabel40@gmail.com>
2025-04-28 13:40:23 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,7 +10,16 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
-    /* to be implemented in part (a) */
+   lights = new boolean[numRows][numCols];
+    for(int r = 0; r < lights.length; r++){
+      for(int c = 0; c < lights[r].length; c++){
+        if(Math.random() < 0.4){
+          lights[r][c]=true;
+        }else{
+          lights[r][c]=false;
+        }
+      }
+    }
 
   }
 
@@ -20,7 +29,20 @@ public class LightBoard
    */
   public boolean evaluateLight(int row, int col)
   {
-    /* to be implemented in part (b) */
+    int on = 0;
+    for(int i = 0; i < light.length; i ++){
+      if(light[i][col]==true){
+        on++;
+      }
+    }
+
+    if(light[row][col] == true && on%2 == 0){
+      return false;
+    }else if(light[row][col] == false && on%3 == 0){
+      return true;
+    }else{
+      return light[row][col];
+    }
    
  
   }
